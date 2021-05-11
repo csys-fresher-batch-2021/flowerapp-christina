9fe2825b631eb8df40b8746d00ada42162b47c2e
Christykumar <christykumar13@gmail.com>
2021-05-11 18:26:24 +0530

    Sonar code smells cleared

--- src/main/java/in/bloomapp/service/FlowerManager.java
+++ src/main/java/in/bloomapp/service/FlowerManager.java
@@ -6,19 +6,23 @@ import java.util.List;
 import in.bloomapp.model.Flower;
 
 public class FlowerManager {
-	public static final String natural="Natural";
-	public static final String artificial="Artiicial";
+	public static final String NATURAL="Natural";
+	public static final String ARTIFICIAL="Artiicial";
 	public static final List<Flower> flowers = new ArrayList<>();
+	
+	private FlowerManager() {
+		
+	}
 
 	// Statically available flowers as appended statically
 
 	static {
-		Flower f1 = new Flower(natural, "Normal Flowers", 300);
-		Flower f2 = new Flower(natural, "Floral Boquet", 1000);
-		Flower f3 = new Flower(natural, "Full decoration package", 10000);
-		Flower f4 = new Flower(artificial, "Normal Flowers", 100);
-		Flower f5 = new Flower(artificial, "Floral Boquet", 600);
-		Flower f6 = new Flower(artificial, "Full decoration package", 8000);
+		Flower f1 = new Flower(NATURAL, "Normal Flowers", 300);
+		Flower f2 = new Flower(NATURAL, "Floral Boquet", 1000);
+		Flower f3 = new Flower(NATURAL, "Full decoration package", 10000);
+		Flower f4 = new Flower(ARTIFICIAL, "Normal Flowers", 100);
+		Flower f5 = new Flower(ARTIFICIAL, "Floral Boquet", 600);
+		Flower f6 = new Flower(ARTIFICIAL, "Full decoration package", 8000);
 		
 		flowers.add(f1);
 		flowers.add(f2);
